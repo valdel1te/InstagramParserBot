0fe5e6b1eefc1d08cdff62d21d112b7441f3eb09
gravityLies <rykov.vladislav99hp@gmail.com>
2022-08-14 16:36:16 +0500

    created base architecture

---
+++ InstagramParserBot/Bot/Telegram/Handlers.cs
@@ -0,0 +1,44 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+
+namespace InstagramParserBot.Bot.Telegram;
+
+public static class Handlers
+{
+    public static Task PollingErrorHandler(
+        ITelegramBotClient botClient,
+        Exception exception,
+        CancellationToken cancellationToken
+    )
+    {
+        var errorMessage = exception switch
+        {
+            ApiRequestException apiRequestException =>
+                $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n[{apiRequestException.Message}]",
+            
+            _ => exception.ToString()
+        };
+        
+        Console.WriteLine(errorMessage);
+        return Task.CompletedTask;
+    }
+
+    public static async Task HandleUpdateAsync(
+        ITelegramBotClient botClient,
+        Update update,
+        CancellationToken cancellationToken
+    )
+    {
+        // var handler = update.Type switch
+        // {
+        //     UpdateType.Message =>
+        // }
+
+        Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: update.Message.Chat.Id,
+            text: update.Message.Text,
+            cancellationToken: cancellationToken
+        );
+    }
+}
